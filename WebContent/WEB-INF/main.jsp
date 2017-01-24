39eaba2bef9c4881fdf46f9666b035d58c99908d
S-W-Williams <swwillia@uci.edu>
2017-01-23 23:33:42 -0800

    jsp page setup for main

--- WebContent/WEB-INF/main.jsp
+++ WebContent/WEB-INF/main.jsp
@@ -1,5 +1,25 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-<body>
-<b>First Name: ${username}<br/>
-</body>
+	<head>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	    <link href="${pageContext.request.contextPath}/CSS/narrow-jumbotron.css" rel="stylesheet">
+		<title>Fabflix</title>
+	</head>
+	<body>
+		<div class="container">
+			<%@ include file="../navbar.jsp" %>
+		</div>
+		<div class="container-fluid">
+		<div class="row">
+			<div class="col-12">
+				<div class="jumbotron">
+				<h1 class="display-3">Welcome ${username}!</h1>
+		    </div>
+			</div>
+		</div>
+		</div>
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	</body>
 </html>
--- src/Fabflix/HomeServlet.java
+++ src/Fabflix/HomeServlet.java
@@ -12,29 +12,28 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class HomeServlet extends HttpServlet{
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
     	
-        HttpSession session = request.getSession(false);  
-        if (session == null) {  
-    		String message = "Please login first!";
-    		request.setAttribute("message", message);
-    		request.getRequestDispatcher("login.jsp").forward(request,response);
-        }
-        else {
-        	String username = (String) session.getAttribute("username");
-        	request.setAttribute("username", username);
-     		request.getRequestDispatcher("/WEB-INF/main.jsp").forward(request,response);
-        }  
+    	try {
+	        HttpSession session = request.getSession(false);  
+	        if (session == null) {  
+	    		String message = "Please login first!";
+	    		request.setAttribute("message", message);
+	    		request.getRequestDispatcher("login.jsp").forward(request,response);
+	        }
+	        else {
+	        	//String username = (String) session.getAttribute("username");
+	        	//request.setAttribute("username", username);
+	     		request.getRequestDispatcher("/WEB-INF/main.jsp").forward(request,response);
+	        }  
+    	} catch (Exception e) {
+    		
+    	}
         
     }
     
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-    	try {
-    		doPost(request, response);
-    	} catch (Exception e) {
-    		
-    	}
+    	doPost(request, response);
     }
 }
