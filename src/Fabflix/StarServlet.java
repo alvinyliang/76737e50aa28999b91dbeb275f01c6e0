9e0e240fb69838eaefc5bc6b516ab579e73d463f
Yiqiao Zhao <zhaoyq0223@gmail.com>
2017-02-02 20:02:33 -0800

    Setup Single Star class

---
+++ src/Fabflix/StarServlet.java
@@ -0,0 +1,22 @@
+package Fabflix;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class StarServlet extends HttpServlet {
+
+}
